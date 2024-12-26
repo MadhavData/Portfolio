1e3d8f9de2c4d891ae4a8d73eb7ed11508eba7f1
Madhav Paluru <madhavpaluru1904@gmail.com>
2024-12-26 13:19:51 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+
+// Navigation Scroll Animation
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
